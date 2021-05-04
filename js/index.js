6a474996aba55a7b4adeaa8fac695cf232cb512a
Oleksandr Huliakov <alexrazor1337@gmail.com>
2021-05-04 18:48:24 +0300

    d2

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
+// let A = [
+//     [2, 1, 1],
+//     [1, -1, 0],
+//     [3, -1, 2]
+// ];
+
+// let B = [2, -2, 2];
+
+// let A = [
+//     [2, 5, 4],
+//     [1, 3, 2],
+//     [2, 10, 9]
+// ];
+
+// let B = [30, 150, 110];
+
 let A = [
-    [2, 1, 1],
-    [1, -1, 0],
-    [3, -1, 2]
+    [1, 1, 1],
+    [4, 2, 1],
+    [9, 3, 1]
 ];
 
-let B = [2, -2, 2];
+let B = [0, 1, 3];
 
 function cpy(obj) {
     return JSON.parse(JSON.stringify(obj))
@@ -46,7 +62,31 @@ function det(matrix) {
     }
 }
 
-function Cramer(A, B) { // проверить сходимость, размерность, ...
+function is_diagonally_dominant(A) {
+    for (let i = 0; i < A.length; i++) {
+        if (Math.abs(A[i][i]) < A[i].slice(i).reduce((a,b) => a + b)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function is_zeros_on_main_diagonal(A) {
+    for (let i = 0; i < A.length; i++) {
+        if (A[i][i] == 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function is_symmetrical(A) {
+
+}
+
+function Cramer(A, B) { // TODO проверить сходимость, размерность, ...
     let main_det = det(A);
     let X = new Array(A.length);
 
@@ -62,9 +102,8 @@ function Cramer(A, B) { // проверить сходимость, размер
     return X;
 }
 
-function Gauss(A, B) {
+function Gauss(A, B) { //TODO Переделать, добавить прове https://www.codesansar.com/numerical-methods/gauss-elimination-method-pseudocode.htm
     let X = new Array(A.length);
-    let tmp = cpy(B);
     for (let k = 0; k < A.length - 1; k++) {
         // Partial pivot
         let max = Math.abs(A[k][k]);
@@ -77,9 +116,9 @@ function Gauss(A, B) {
         }
 
         if (m != k) {
-            let b_temp = tmp[k];
-            tmp[k] = tmp[m];
-            tmp[m] = b_temp;
+            let b_temp = B[k];
+            B[k] = B[m];
+            B[m] = b_temp;
             for (let j = k; j < A.length; j++) {
                 let A_temp = A[k][j];
                 A[k][j] = A[m][j];
@@ -92,13 +131,14 @@ function Gauss(A, B) {
             for (let j = k + 1; j < A.length; j++) {
                 A[i][j] -= factor * A[k][j];
             }
-            tmp[i] -= factor*tmp[k];
+            B[i] -= factor*B[k];
         }
     }
 
+
     // Back substitution
     for (let i = A.length-1; i >= 0; i--) {
-        X[i] = tmp[i];
+        X[i] = B[i];
         for (let j = i + 1; j < A.length; j++) {
             X[i] -= A[i][j] * X[j];
         }
@@ -109,5 +149,127 @@ function Gauss(A, B) {
 }
 
 
-console.log(Cramer(cpy(A), cpy(B)));
-console.log(Gauss(cpy(A), cpy(B)));
+
+function Seidel(A, B, q = 0.001) { // TODO Добавить счетчик итераций?
+    let X = new Array(A.length).fill(0);
+    while (true) {
+        let old_X = cpy(X);
+        for (let i = 0; i < A.length; i++) {
+            let S = 0;
+            for (let j = 0; j < A.length; j++) {
+                if (i != j) {
+                    S += A[i][j] * X[j];
+                }
+            }
+            X[i] = (1 / A[i][i]) * (B[i] - S)
+        }
+
+        let convergence = true;
+        for (let i = 0; i < X.length; i++) {
+            if (Math.abs(X[i] - old_X[i]) > q) {
+                convergence = false;
+                break;
+            }
+        }
+
+        if (convergence) {
+            return X;
+        }
+    }
+}
+
+function Gauss_Jordan(A, B) { // TODO Переделать https://www.codesansar.com/numerical-methods/gauss-jordan-method-pseudocode.htm#
+    let X = new Array(A.length);
+    for (let k = 0; k < A.length - 1; k++) {
+        // Partial pivot
+        let max = Math.abs(A[k][k]);
+        let m = k;
+        for (let i = k + 1; i < A.length; i++){ //
+            if (Math.abs(A[i][k]) > max) {
+                max = Math.abs(A[i][k]);
+                m = i;
+            }
+        }
+
+        if (m != k) {
+            let b_temp = B[k];
+            B[k] = B[m];
+            B[m] = b_temp;
+            for (let j = k; j < A.length; j++) {
+                let A_temp = A[k][j];
+                A[k][j] = A[m][j];
+                A[m][j] = A_temp;
+            }
+        }
+        // Forward elimination
+        for (let i = k + 1; i < A.length; i++) {
+            let factor = A[i][k]/A[k][k];
+            for (let j = k + 1; j < A.length; j++) {
+                A[i][j] -= factor * A[k][j];
+            }
+            B[i] -= factor*B[k];
+        }
+    }
+
+
+    // Back substitution
+    for (let i = A.length-1; i >= 0; i--) {
+        X[i] = B[i];
+        for (let j = i + 1; j < A.length; j++) {
+            X[i] -= A[i][j] * X[j];
+        }
+        X[i] /= A[i][i];
+    }
+
+    return X;
+}
+
+function Jacobi(A, B, q = 0.01) { // TODO Добавить счетчик итераций?
+    let X = new Array(A.length).fill(0);
+    let iter = 0;
+    while (iter < 1000) {
+        let old_X = cpy(X);
+        for (let i = 0; i < A.length; i++) {
+            let S = 0;
+            for (let j = 0; j < A.length; j++) {
+                if (i != j) {
+                    S += A[i][j] * old_X[j];
+                }
+            }
+            X[i] = (1 / A[i][i]) * (B[i] - S)
+        }
+
+        let convergence = true;
+        for (let i = 0; i < X.length; i++) {
+            if (Math.abs(X[i] - old_X[i]) > q) {
+                convergence = false;
+                break;
+            }
+        }
+
+        if (convergence) {
+            return X;
+        }
+
+        iter++;
+    }
+
+    return X;
+}
+
+function solve(A, B) {
+    console.log(Cramer(cpy(A), cpy(B)));
+    console.log(Gauss(cpy(A), cpy(B)));
+    if (is_diagonally_dominant(A)) {
+        console.log(Seidel(cpy(A), cpy(B)));
+    } else {
+        console.error("Can't do it with Seidel!");
+    }
+    console.log(Gauss_Jordan(cpy(A), cpy(B)));
+    if (is_diagonally_dominant(A) && !is_zeros_on_main_diagonal(A)) {
+        console.log(Jacobi(cpy(A), cpy(B)));
+    } else {
+        console.error("Can't do it with Jacobi, zeros on the main diagonal!");
+    }
+}
+solve(A, B);
